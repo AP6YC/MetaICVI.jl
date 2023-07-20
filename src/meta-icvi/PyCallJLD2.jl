410191f1b38190f5afa6557e4b74e2d05e24a839
Sasha Petrenko <sap625@umsystem.edu>
2023-07-20 14:49:53 -0500

    Dev reentrant lock for pycalls

--- src/meta-icvi/PyCallJLD2.jl
+++ src/meta-icvi/PyCallJLD2.jl
@@ -13,10 +13,15 @@ using PyCall, JLD2
 const dumps = PyNULL()
 const loads = PyNULL()
 
+# Lock during pycalls
+# https://github.com/JuliaPy/PyCall.jl/issues/882
+const PYLOCK = Ref{ReentrantLock}()
+
 function __init__()
     pickle = pyimport(PyCall.pyversion.major ≥ 3 ? "pickle" : "cPickle")
     copy!(dumps, pickle.dumps)
     copy!(loads, pickle.loads)
+    PYLOCK[] = ReentrantLock()
 end
 
 struct PyObjectSerialization
@@ -25,6 +30,8 @@ end
 
 JLD2.writeas(::Type{PyObject}) = PyObjectSerialization
 
+pylock(f::Function) = Base.lock(f, PYLOCK[])
+
 function JLD2.wconvert(::Type{PyObjectSerialization}, pyo::PyObject)
     # __init__()
     b = PyCall.PyBuffer(pycall(dumps, PyObject, pyo))
@@ -32,32 +39,39 @@ function JLD2.wconvert(::Type{PyObjectSerialization}, pyo::PyObject)
     # We need a `copy` here because the PyBuffer might be GC'ed after we've
     # left this scope, but see
     # https://github.com/JuliaPy/PyCallJLD.jl/pull/3/files/17b052d018f79905baf855b40e440d2cacc171ae#r115525173
-    return PyObjectSerialization(
-        copy(
-            unsafe_wrap(
-                Array,
-                Ptr{UInt8}(pointer(b)),
-                sizeof(b)
+    serialized = pylock() do
+        PyObjectSerialization(
+            copy(
+                unsafe_wrap(
+                    Array,
+                    Ptr{UInt8}(pointer(b)),
+                    sizeof(b)
+                )
             )
         )
-    )
+    end
+
+    return serialized
 end
 
 function JLD2.rconvert(::Type{PyObject}, pyo_ser::PyObjectSerialization)
-    # __init__()
-    return pycall(
-        loads,
-        PyObject,
-        PyObject(
-            PyCall.@pycheckn ccall(
-                @pysym(PyCall.PyString_FromStringAndSize),
-                PyPtr,
-                (Ptr{UInt8}, Int),
-                pyo_ser.repr,
-                sizeof(pyo_ser.repr)
+    reconstructed = pylock() do
+        pycall(
+            loads,
+            PyObject,
+            PyObject(
+                PyCall.@pycheckn ccall(
+                    @pysym(PyCall.PyString_FromStringAndSize),
+                    PyPtr,
+                    (Ptr{UInt8}, Int),
+                    pyo_ser.repr,
+                    sizeof(pyo_ser.repr)
+                )
             )
         )
-    )
+    end
+
+    return reconstructed
 end
 
 end
